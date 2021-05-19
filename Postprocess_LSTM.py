eff544cb4d0f5fc9cac27b0d003b39a68d32f60e
sophieaylin <sophie-aylin@web.de>
2021-05-19 09:11:08 +0200

    denormalization with Pdc.max()

--- Postprocess_LSTM.py
+++ Postprocess_LSTM.py
@@ -3,8 +3,7 @@ import numpy as np
 import statistics
 from DataManagement import get_target_LSTM
 
-# Postprozess
-# NOTE: if horizon is changed
+# Postprocess
 
 def computeMetrics(path):
 
@@ -12,12 +11,10 @@ def computeMetrics(path):
     mae = file["MAE"]
     mbe = file["MBE"]
     rmse = file["RMSE"]
-    # rmse_sp = file["RMSE_sp"]
 
     MAE = []
     MBE = []
     RMSE = []
-    # RMSE_sp = []
     skill = []
 
     for line in range(len(file)):
@@ -32,9 +29,12 @@ def computeMetrics(path):
         MBE.append(row_mbe)
         RMSE.append(row_rmse)
 
-    for i in range(len(rmse_sp)):
-        val = 1 - (RMSE[i]/rmse_sp[i])
-        skill.append(val)
+    for i in range(len(RMSE)):
+        row = []
+        for h in range(len(rmse_sp)):
+            val = 1 - (RMSE[i][h]/rmse_sp[h])
+            row.append(val)
+        skill.append(row)
 
     mae_mean = np.nanmean(MAE, axis=0)
     mbe_mean = np.nanmean(MBE, axis=0)
@@ -44,31 +44,36 @@ def computeMetrics(path):
 
     for t in range(len(mae_mean)-1):
         horizon = (t+1) * 5
-        print("forecast {}min: MAE: {} std: {}, MBE: {} std: {}, RMSE: {} std: {}, skill: {} std: {}".format
-              (horizon, mae_mean[t], statistics.stdev(MAE[t]), mbe_mean[t], statistics.stdev(MBE[t]),
-               rmse_mean[t], statistics.stdev(RMSE[t]), skill_mean[t] * 100, statistics.stdev(MAE[t])))
+        print("forecast {}min: MAE: {:.2f} std: {:.1f}, MBE: {:.2f} std: {:.1f}, RMSE: {:.2f} std: {:.1f}, skill: {:.2f} std: {:.1f}".format
+              (horizon, mae_mean[t], np.nanstd(np.transpose(MAE)[t]), mbe_mean[t], np.nanstd(np.transpose(MBE)[t]),
+               rmse_mean[t], np.nanstd(np.transpose(RMSE)[t]), skill_mean[t] * 100, np.nanstd(np.transpose(skill)[t])))
 
     return None
 
-path = "D:/TU_Stuttgart/Studienarbeit/LSTM_results/metricLSTM_m2m_layer_2_Input_41_hidden_100_0_cr.csv"
-# metricLSTM_layer_2_Input_1591_hidden_100
-# metricLSTM_m2m_layer_2_Input_60_hidden_100_0
-# metricLSTM_m2m_layer_2_Input_41_hidden_100_0_cr
-# metricLSTM_m2m_layer_10_Input_23_hidden_100_0_cr_bt12
+path = "D:/TU_Stuttgart/Studienarbeit/LSTM_results/metricLSTM_m2m_layer_2_Input_8_hidden_100_0_seas_shift_Ind_denorm.csv"
+# metricLSTM_m2m_layer_2_Input_5_hidden_100_highneg_cr, 100 epochen
+# metricLSTM_m2m_layer_2_Input_5_hidden_100_0_cr
+# metricLSTM_m2m_layer_2_Input_5_hidden_100_highneg_seas
+# metricLSTM_m2m_layer_2_Input_5_hidden_100_0_seas, 100 epochen
+# metricLSTM_m2m_layer_2_Input_3_hidden_100_0_seas_gtiBNIRHkd_sc
+# metricLSTM_m2m_layer_2_Input_5_hidden_100_0_seas_minmax
+# metricLSTM_m2m_layer_2_Input_8_hidden_100_0_seas_shift_Ind
+# metricLSTM_m2m_layer_2_Input_8_hidden_100_0_seas_Ind
+
 target = get_target_LSTM()
 target.insert(target.shape[1], column="key", value = np.array(range(0,len(target))))
 train_y = target[target["dataset"] == "Train"]
 test_y = target[target["dataset"] == "Test"]
 
-# Pdc_sp_train = train_y["Pdc_sp"]
-# Pdc_sp_test = test_y["Pdc_sp"]
+test_y = test_y.drop(test_y.index[test_y["El"] < 15])
 
 tar_cols = test_y.filter(regex="min").columns.tolist()
 
 rmse_sp = []
 for step in range(0, len(tar_cols)):
     min = tar_cols[step]
-    error_sp = np.sqrt(np.nanmean((test_y["Pdc_sp"] - test_y["{}".format(min)]).values ** 2))
+    Pdc_act = test_y["{}".format(min)] * test_y["Pdc_33"].max() # test_y["ENI"]
+    error_sp = np.sqrt(np.nanmean((Pdc_act - test_y["Pdc_sp"]).values ** 2))
     rmse_sp.append(error_sp)
 
 computeMetrics(path)
