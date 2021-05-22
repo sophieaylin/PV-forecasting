ce37f22101d16b6b6a5e881a4cfe71f9e569cefc
sophieaylin <sophie-aylin@web.de>
2021-05-23 01:52:05 +0200

    dropout layer inserted

--- LSTMModel.py
+++ LSTMModel.py
@@ -12,6 +12,8 @@ class LSTM(torch.nn.Module):
         # batch_first=True causes input/output tensors to be of shape
         # (batch_dim, seq_dim, feature_dim)
         self.lstm = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True)
+        # Dropout layer
+        self.drop = nn.Dropout(p=0.5)
         # Readout layer
         self.fc = nn.Linear(hidden_dim, output_dim)
 
@@ -29,14 +31,18 @@ class LSTM(torch.nn.Module):
         # If we don't, we'll backprop all the way to the start even after going through another batch
         out, (h_n, c_n) = self.lstm(x, (h_0.detach(), c_0.detach())) # why detach?
 
+        outd = self.drop(out)
+
         # Index hidden state of last time step
         # out.size() --> 100, 28, 100
         # out[:, -1, :] --> 100, 100 --> just want last time step hidden states!
-        out = self.fc(out[:, -1, :])
+
+        outd = self.fc(outd[:, -1, :])
+        # out = self.fc(out[:, -1, :])
         # out.size() --> 100, 10
         """print('The shape of lstm_out is:', out.shape)  # (batch_size, seq_len, hidden_dim)
         print('The shape of h is:', h_n.shape)"""
-        return out
+        return outd
 
     def loss(self, x, y):
         loss = torch.sqrt(self.criterion(x, y))
